b40397b3abf17aa5b85d1718c295d44c52b65f50
Раевский Федор Дмитриевич <raevskiifd.24@sch239.net>
2023-09-28 10:47:12 +0300

    first

---
+++ Practic2023/target/debug/deps/Practic2023-1523cf74eded8d89.d
@@ -0,0 +1,5 @@
+H:\RustProgramms\InfClasses2023\Practic2023\target\debug\deps\Practic2023-1523cf74eded8d89.rmeta: src\main.rs
+
+H:\RustProgramms\InfClasses2023\Practic2023\target\debug\deps\Practic2023-1523cf74eded8d89.d: src\main.rs
+
+src\main.rs:
---
+++ Practic2023/target/debug/deps/Practic2023-3c5396eed251a870.d
@@ -0,0 +1,5 @@
+H:\RustProgramms\InfClasses2023\Practic2023\target\debug\deps\Practic2023-3c5396eed251a870.rmeta: src\main.rs
+
+H:\RustProgramms\InfClasses2023\Practic2023\target\debug\deps\Practic2023-3c5396eed251a870.d: src\main.rs
+
+src\main.rs:
